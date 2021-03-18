c020326347e6255e38474fe4d60d75724b03ac4d
RMCCorporate <tcorrea@rmc.cl>
2021-03-18 19:54:29 -0300

    Vista planificador lista

--- proyectos/urls.py
+++ proyectos/urls.py
@@ -4,4 +4,9 @@ from proyectos import views
 
 urlpatterns = [
     path('planificador', views.planificador, name='planificador'),
+    path('lista_productos', views.recibir_datos_planificador, name='lista_productos'),
+    path('lista_proveedores', views.recibir_cantidades_planificador, name='lista_proveedores'),
+    path('lista_correos', views.enviar_correos, name='lista_correos'),
+
+
 ]
--- proyectos/views.py
+++ proyectos/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from planificador.models import Clase, SubClase
+from planificador.models import Clase, SubClase, Producto, Proveedor
+from django.http import HttpResponse
 
 #Funciones:
 def clases_lista_productos(clase):
@@ -13,7 +14,8 @@ def clases_lista_productos(clase):
         subclase_aux.append(productos_aux)
         sub_clase_general.append(subclase_aux)
     return sub_clase_general
-# Create your views here.
+
+# Vista planificador I
 def planificador(request):
     clases = Clase.objects.all()
     subclases = []
@@ -27,6 +29,64 @@ def planificador(request):
     clase1 = clases_lista_productos(subclases[0])
     clase2 = clases_lista_productos(subclases[1])
     clase3 = clases_lista_productos(subclases[2])
-   
-    
     return render(request, "proyectos/planificador.html", {"Nombre1":nombres[0], "Subclases1":clase1, "Nombre2":nombres[1], "Subclases2":clase2, "Nombre3":nombres[2], "Subclases3":clase3})
+
+#Recibir vista planificador I
+def recibir_datos_planificador(request):
+    id = request.GET["id"]
+    nombre = request.GET["nombre"]
+    tipo_cambio = request.GET["tipo_cambio"]
+    valor_cambio = request.GET["valor_cambio"]
+    fecha_inicio = request.GET["fecha_inicio"]
+    fecha_termino = request.GET["fecha_termino"]
+    productos = request.GET.getlist("subclase")
+    lista_subclases_productos = []
+    for producto in productos:
+        lista_aux_producto = []
+        lista_aux_proveedores = []
+        instancia_producto = Producto.objects.get(nombre=producto)
+        lista_aux_producto.append(instancia_producto)
+        subclases_producto = instancia_producto.subclase_set.all()
+        subclases_proveedores = Proveedor.objects.filter(subclases_asociadas=subclases_producto[0])
+        for i in subclases_proveedores:
+            lista_aux_proveedores.append(i)
+        lista_aux_producto.append(lista_aux_proveedores)
+        subclases_proveedores = []
+        lista_subclases_productos.append(lista_aux_producto)
+    return render(request, "proyectos/lista_productos.html", {"Productos":lista_subclases_productos})
+
+def recibir_cantidades_planificador(request):
+    numero_productos = int(request.GET["numero_productos"])
+    lista_separada = []
+    #Lista separada = Proveedores
+    for i in range(numero_productos):
+        a = request.GET.getlist("proveedor{}".format(str(i)))
+        lista_separada.append(a)
+    join = []
+    for i in lista_separada:
+        join += i
+    proveedores = list(dict.fromkeys(join))
+    cantidad = request.GET.getlist("cantidad")
+    productos = request.GET.getlist("id_producto")
+    lista_general_proveedores = []
+    lista_final_final = []
+    for proveedor in proveedores:
+        lista_final = []
+        lista_final.append(proveedor)
+        lista_aux2 = []
+        for counter, i in enumerate(lista_separada):
+            lista_aux = []
+            for k in i:
+                if k == proveedor:
+                    lista_aux.append(productos[int(counter)])
+                    lista_aux.append(cantidad[counter])
+                    lista_aux2.append(lista_aux)
+        lista_final.append(lista_aux2)
+        lista_general_proveedores.append(lista_final)
+
+    print(lista_general_proveedores)
+    mensaje = "CACA"
+    return render(request, "proyectos/lista_proveedores.html", {"Productos":lista_general_proveedores})
+
+def enviar_correos(request):
+    return HttpResponse("NO SE HA HECHO ESTA PARTE")
